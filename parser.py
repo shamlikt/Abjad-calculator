aa73f4b7abe40443d581f4e59bfa77360339fc6c
shamlik <tshamlikt@gmail.com>
2016-05-03 13:19:39 +0530

    changes pareser to functions

--- parser.py
+++ parser.py
@@ -1,31 +1,39 @@
 # -*- coding=utf-8 -*-
+
 from pyarabic.unshape import unshaping_text
 from pyarabic.araby import strip_tashkeel
-from word import letter as key
+import database
+
 input_text = unicode("1,بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ",'utf-8')
-#input_text = "خَلِيفَةً"
-stripped_text =  strip_tashkeel(input_text)
-space_count = stripped_text.count(' ')
-stripped_text = stripped_text.replace(' ','',space_count)
-stripped_text = stripped_text.rstrip()
-letter =  unshaping_text(stripped_text)
+def strip(input_text):
+     return  strip_tashkeel(input_text)
 
-for i in letter:
-    print i
-uni_key = {}
-for i in key.keys():
-    uni_key[unicode(i,'utf-8')] = key[i]
+def unshape(stripped_text):
+    stripped_text = unshaping_text(stripped_text)
+    return stripped_text.replace('\n','',stripped_text.count('\n'))
 
-sum_value = 0
-for i in letter:
-    try:
-        sum_value = sum_value +uni_key[i]
-    except:
-        print "skip"
-        print str(i)
+def format_text(stripped_text):
+    space_count = stripped_text.count(' ')
+    stripped_text = stripped_text.replace(' ','',space_count)
+    return stripped_text.replace('\n','',stripped_text.count('\n'))
 
-print sum_value
-# import pdb;pdb.set_trace()
-# print list(letter)
-# print key.keys()
-# print sum_value
+def parse(key,letter):
+    uni_key = {}
+    uni_key.update(key)
+    sum_value = 0
+    skip_letter = []
+    for i in letter:
+        try:
+            sum_value = sum_value +uni_key[i]
+        except:
+            skip_letter.append(i)
+    return sum_value,skip_letter
+
+
+db,cu = database.connect_db()
+database.insert_initial(db,cu)
+key = database.fetch_data('one',cu)
+stripped_text =  strip(input_text)
+format_string = format_text(stripped_text)
+letter = unshape(format_string)
+print parse(key,letter)
