347b2a2bd258379b17b8f45ebf07013eee308f7a
shamlik <tshamlikt@gmail.com>
2016-05-03 13:19:07 +0530

    Changes database action to functions

---
+++ abjid/abjid.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-05-02T13:05:47
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = abjid
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
--- database.py
+++ database.py
@@ -1,8 +1,69 @@
+# -*- coding=utf-8 -*-
 import sqlite3
+from collections import OrderedDict
 
-db = sqlite3.connect('data')
-cu = db.cursor()
-cu.execute('''CREATE TABLE abjid(id INTIGER PRIMARY KEY,letter TEXT,
-            value INTIGER)''')
-db.commit()
+def connect_db():
+    db = sqlite3.connect('data')
+    cu = db.cursor()
+    try:
+        cu.execute('''CREATE TABLE abjid(id INTIGER PRIMARY KEY,letter TEXT,
+        value INTIGER, label TEXT)''')
+    except:
+        cu.execute(''' DROP TABLE abjid ''')
+        cu.execute('''CREATE TABLE abjid(id INTIGER PRIMARY KEY,letter TEXT,
+        value INTIGER, label TEXT)''')
+    return db,cu
+    
+def insert_initial(db,cu):
+    letter = OrderedDict()
+    letter.update([(
+    'ا',1),(
+    'ب',2),(
+    'ة',5),(
+    'ت',400),(
+    'ث',500),(
+    'ج',3),(
+    'ح',8),(
+    'خ',600),(
+    'د',4),(
+        'ذ',700),(
+        'ر',200),(
+        'ز',7),(
+        'س',60),(
+        'ش',300),(
+        'ص',90),(
+        'ض',800),(
+        'ط',9),(
+        'ظ',900),(
+        'ع',70),(
+        'غ',100),(
+        'ؽ',1),(
+        'ف',80),(
+        'ق',100),(
+        'ك',20),(
+        'ل',30),(
+        'م',40),(
+        'ن',50),(
+        'ه',5),(
+        'و',6),(
+        'ى',10),(
+        'ي',10),(
+        'ء',1),(
+        'أ',1),(
+        'إ',1),(
+        'آ',1),(
+        'ؤ',1)])
+
+    for i in letter:
+        cu.execute('''INSERT INTO abjid(letter,value,label) values(?,?,?)''',(unicode(i,'utf-8'),letter[i],'one'))
+    db.commit()
+
+def fetch_data(label,cu):    
+    data = cu.execute("SELECT letter,value FROM abjid WHERE label=?",(label,))
+    return data
+
+def insert_data(letter,value,label,cu):
+    cu.execute('''INSERT INTO abjid(letter,value,label) values(?,?,?)''',(unicode(letter,'utf-8'),value,label))
+    db.commit()
+
 
